821b1cb4307a38218343d154b86cb7d5c80ebac3
nicolas <nouno@DESKTOP-HQSJ8UF>
2024-10-04 16:52:39 +0200

    deplacement ligne.

--- Calculs/Program.cs
+++ Calculs/Program.cs
@@ -10,6 +10,7 @@ namespace Calculs
         static void Main(string[] args)
         {
             // variables 
+            
             Random rand = new Random(); // outil de génération de nombre aléatoire
             int val1, val2; // mémorisation de nombres aléatoires
             int solution; // calcul de la solution
